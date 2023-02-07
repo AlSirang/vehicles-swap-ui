29ca7e87a5b8c02867905365072349499fcae2c8
AlSirang <uddinbashir1999@gmail.com>
2023-02-07 16:50:53 +0500

    fix: update user header

--- src/components/header/user.header.jsx
+++ src/components/header/user.header.jsx
@@ -1,7 +1,5 @@
-import { Link } from "react-router-dom";
 import NavbarMain from "./main";
 import { Nav } from "react-bootstrap";
-import { ArrowRight } from "react-bootstrap-icons";
 
 export default function AuthHeader() {
   return (
