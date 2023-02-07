ca403b6c91bfb4e97da25679b8323108ab49172f
AlSirang <uddinbashir1999@gmail.com>
2023-02-07 11:07:09 +0500

    fix: create layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <div className="App">hello</div>;
-}
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorBoundary from "components/errorBundary";
+import Suspense from "components/suspense";
+const Auth = React.lazy(() => import("pages/auth"));
+const Home = React.lazy(() => import("pages/home"));
 
-export default App;
+export default function App() {
+  return (
+    <RouterProvider
+      router={createBrowserRouter(
+        createRoutesFromElements(
+          <Route
+            errorElement={<ErrorBoundary />}
+            element={
+              <Suspense>
+                <Outlet />
+              </Suspense>
+            }
+          >
+            <Route element={<Home />} path="/" />
+            <Route element={<Auth />} path="/auth" />
+          </Route>
+        )
+      )}
+    />
+  );
+}
---
+++ src/components/suspense.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Suspense({ children }) {
+  return <React.Suspense fallback={<Loading />}>{children}</React.Suspense>;
+}
+
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
+}
