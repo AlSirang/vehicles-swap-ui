e628a73de361b1b73444a2ab38f6c6974cd2024e
AlSirang <uddinbashir1999@gmail.com>
2023-02-09 20:38:28 +0500

    fix: update suspense layout

--- src/layout/suspense.layout.jsx
+++ src/layout/suspense.layout.jsx
@@ -2,13 +2,17 @@ import Header from "components/header";
 import React from "react";
 import { Container } from "react-bootstrap";
 import { ArrowClockwise } from "react-bootstrap-icons";
+import { useInitializePackages } from "src/hooks/useWalletContext";
 
 export default function SuspenseLayout({ children }) {
+  useInitializePackages();
   return (
     <>
       <Header />
       <React.Suspense fallback={<Loading />}>
-        <Container fluid>{children}</Container>
+        <Container fluid className="layout-container">
+          {children}
+        </Container>
       </React.Suspense>
     </>
   );
@@ -18,7 +22,7 @@ function Loading() {
   return (
     <div
       className="d-flex flex-column justify-content-center align-items-center"
-      style={{ height: "calc(100vh - 60px)" }}
+      style={{ height: "90vh" }}
     >
       <ArrowClockwise className="loading-animaiton" size="60" />
       <h4>Loading...</h4>
