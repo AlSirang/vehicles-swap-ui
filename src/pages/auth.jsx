c639043fa0a87d25b5078e37bfc7217a22647b26
AlSirang <uddinbashir1999@gmail.com>
2023-02-09 21:20:59 +0500

    fix: implement connect wallet function

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,16 +1,36 @@
+import { useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { Wallet } from "react-bootstrap-icons";
+import { useNavigate } from "react-router-dom";
+import { WalletUserContext } from "src/context";
+import { useWalletContext } from "src/hooks";
 
 export default function SignIn() {
+  const navigation = useNavigate();
+  const { walletConnect } = useWalletContext();
+  const { state } = WalletUserContext();
+
+  const onWalletConnect = async () => {
+    try {
+      await walletConnect();
+    } catch (err) {}
+  };
+
+  const { isWalletConnected } = state;
+  useEffect(() => {
+    isWalletConnected && navigation("/");
+  }, [isWalletConnected, navigation]);
+
   return (
     <div
       className="d-flex justify-content-center align-items-center"
-      style={{ height: "calc(100vh - 60px)" }}
+      style={{ height: "80vh" }}
     >
       <div className="d-flex flex-column justify-content-center">
         <h3 className="d-flex justify-content-center ">Connect your wallet</h3>
 
         <Button
+          onClick={onWalletConnect}
           variant="outline-dark"
           className="d-flex align-items-center justify-content-center gap-3 btn-lg my-3"
         >
